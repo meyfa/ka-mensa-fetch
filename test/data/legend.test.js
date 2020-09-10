8f6eade3a08462cc449d02fef6ebbfc42dc771d6
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2020-09-10 18:36:26 +0200

    Fix wrongly transcribed legend, add test case

---
+++ test/data/legend.test.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const { expect } = require('chai')
+
+const legend = require('../../data/legend.json')
+
+describe('data: legend.json', function () {
+  it('does not have duplicate short values', function () {
+    const counts = new Map()
+    for (const { short } of legend) {
+      counts.set(short, (counts.get(short) || 0) + 1)
+    }
+    for (const [short, count] of counts) {
+      expect(count).to.equal(1, '"' + short + '" occurs ' + count + ' times')
+    }
+    expect()
+  })
+})
