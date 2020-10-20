e3a121f1632371689ef718549039aec4b9271a47
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2020-10-20 16:14:11 +0200

    Add tests for request modules

---
+++ test/handicap/handicap-request.test.js
@@ -0,0 +1,55 @@
+'use strict'
+
+const chai = require('chai')
+chai.use(require('chai-as-promised'))
+const { expect } = chai
+
+const MockAdapter = require('axios-mock-adapter')
+
+const request = require('../../src/handicap/handicap-request.js')
+
+describe('handicap/handicap-request.js', function () {
+  let mock
+
+  beforeEach(function () {
+    mock = new MockAdapter(require('axios'), { onNoMatch: 'throwException' })
+  })
+
+  afterEach(function () {
+    mock.restore()
+    mock = null
+  })
+
+  it('sends request as expected', function () {
+    mock.onAny().replyOnce(config => {
+      expect(config.url).to.equal('https://www.sw-ka.de/de/essen/')
+      expect(config.method).to.equal('get')
+      expect(config.params).to.deep.equal({
+        STYLE: 'popup_plain',
+        view: 'ok',
+        c: 'test-canteen',
+        kw: 11
+      })
+      return [200, { some: { data: 42 } }]
+    })
+    return expect(request('test-canteen', 11))
+      .to.eventually.be.fulfilled
+  })
+
+  it('returns data as-is', function () {
+    mock.onAny().replyOnce(config => {
+      return [200, { some: { data: 42 } }]
+    })
+    return expect(request('test-canteen', 11))
+      .to.eventually.deep.equal({ some: { data: 42 } })
+  })
+
+  it('includes session cookie if provided', function () {
+    mock.onAny().replyOnce(config => {
+      expect(config.headers).to.have.property('Cookie').that.equals('platoCMS=qux42baz')
+      return [200, { some: { data: 42 } }]
+    })
+    return expect(request('test-canteen', 11, 'qux42baz'))
+      .to.eventually.be.fulfilled
+  })
+})
---
+++ test/jsonapi/jsonapi-request.test.js
@@ -0,0 +1,82 @@
+'use strict'
+
+const chai = require('chai')
+chai.use(require('chai-as-promised'))
+const { expect } = chai
+
+const MockAdapter = require('axios-mock-adapter')
+
+const request = require('../../src/jsonapi/jsonapi-request.js')
+
+describe('jsonapi/jsonapi-request.js', function () {
+  let mock
+
+  beforeEach(function () {
+    mock = new MockAdapter(require('axios'), { onNoMatch: 'throwException' })
+  })
+
+  afterEach(function () {
+    mock.restore()
+    mock = null
+  })
+
+  it('contains endpoint constants', function () {
+    expect(request).to.have.property('METADATA_ENDPOINT')
+    expect(request).to.have.property('PLANS_ENDPOINT')
+  })
+
+  it('rejects for missing/wrong endpoint', function () {
+    return Promise.all([
+      expect(request({ user: 'a', password: 'b' })).to.eventually.be.rejected,
+      expect(request({ user: 'a', password: 'b' }, 'invalid')).to.eventually.be.rejected
+    ])
+  })
+
+  describe('with #METADATA_ENDPOINT', function () {
+    it('sends request as expected', function () {
+      mock.onAny().replyOnce(config => {
+        expect(config.url).to.equal('https://www.sw-ka.de/json_interface/general/')
+        expect(config.method).to.equal('get')
+        expect(config.auth).to.deep.equal({
+          username: 'a',
+          password: 'b'
+        })
+        return [200, { some: { data: 42 } }]
+      })
+      return expect(request({ user: 'a', password: 'b' }, request.METADATA_ENDPOINT))
+        .to.eventually.be.fulfilled
+    })
+
+    it('returns data as-is', function () {
+      mock.onAny().replyOnce(config => {
+        return [200, { some: { data: 42 } }]
+      })
+      return expect(request({ user: 'a', password: 'b' }, request.METADATA_ENDPOINT))
+        .to.eventually.deep.equal({ some: { data: 42 } })
+    })
+  })
+
+  describe('with #PLANS_ENDPOINT', function () {
+    it('sends request as expected', function () {
+      mock.onAny().replyOnce(config => {
+        expect(config.url).to.equal('https://www.sw-ka.de/json_interface/canteen/')
+        expect(config.method).to.equal('get')
+        expect(config.auth).to.deep.equal({
+          username: 'a',
+          password: 'b'
+        })
+        return [200, { some: { data: 42 } }]
+      })
+      return expect(request({ user: 'a', password: 'b' }, request.PLANS_ENDPOINT))
+        .to.eventually.be.fulfilled
+    })
+
+    it('returns data as-is', function () {
+      mock.onAny().replyOnce(config => {
+        return [200, { some: { data: 42 } }]
+      })
+      return expect(request({ user: 'a', password: 'b' }, request.PLANS_ENDPOINT))
+        .to.eventually.deep.equal({ some: { data: 42 } })
+    })
+  })
+})
